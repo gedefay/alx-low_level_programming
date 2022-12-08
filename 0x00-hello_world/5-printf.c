bd4d0a850300652fd40060d24ce7b3c33904bfbc
gedefay <gedefayeayana@gmail.com>
2022-12-08 07:37:19 -0800

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-/*
-* main - Begin of execution
-* Return: 0 if success
-*/
-
+/**
+ * main - Begin of execution
+ * Return: 0 if success
+ */
 int main(void)
 
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
-
 }
