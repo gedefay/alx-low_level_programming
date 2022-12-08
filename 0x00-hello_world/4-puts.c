6d84ea5bb54852f932c0d55d16a6409d6dc3e8b3
gedefay <gedefayeayana@gmail.com>
2022-12-08 07:04:49 -0800

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
-/*
- * main - Begin of execution
- * Return: 0 if success
+/**
+ * main - begin of excution
+ *
+ * Rturn: 0 if success
  */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
