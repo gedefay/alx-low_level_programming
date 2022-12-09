c1e2c06443b75f00bd830eb933565a5ec3eae5bb
gedefay <gedefayeayana@gmail.com>
2022-12-09 09:53:40 -0800

    if w

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
+
 /**
- * main - Begin of execution
- * Return: 0 if success
-*/
+ * main - Prints the alphabet without q and e.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-char c = 'a';
+int i;
 
-while (c <= 'z')
+for (i = 97; i < 123; i++)
 {
-if (c != 'e' && c != 'q')
-putchar(c);
-c++;
+if (i != 101 && i != 113)
+{
+putchar(i);
 }
-printf("\n");
-
+}
+putchar('\n');
 return (0);
+}
