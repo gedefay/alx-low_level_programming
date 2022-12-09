d04225a21616170557afc6ae1c33176d93cb589a
gedefay <gedefayeayana@gmail.com>
2022-12-09 07:56:42 -0800

    if ,while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Begin of execution
+ * Return: 0 if success
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+while (c <= 'z')
+{
+	if (c != 'e' && c != 'q')
+		putchar(c);
+	c++;
+}
+printf("\n");
+return (0);
