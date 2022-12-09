d0216700fb75f24da9a45cf221f97c5a315d7a1b
gedefay <gedefayeayana@gmail.com>
2022-12-09 05:27:01 -0800

    if & while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,43 @@
+nclude <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ *
+ *  * main - Begin of execution
+ *
+ *   *
+ *
+ *    * Return: 0 if success
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char c = 'a';
+
+
+
+			while (c <= 'z')
+
+					{
+
+								putchar(c);
+
+										c++;
+
+											}
+
+				printf("\n");
+
+
+
+					return (0);
+
+}
