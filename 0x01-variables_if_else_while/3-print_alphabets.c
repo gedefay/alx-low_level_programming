f42f6e7ef0c82c856a6853cf3c264bf04d6875f4
gedefay <gedefayeayana@gmail.com>
2022-12-09 07:45:57 -0800

    if while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Begin of execution
+ * Return: 0 if success
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
