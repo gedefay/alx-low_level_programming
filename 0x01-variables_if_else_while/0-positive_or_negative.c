fd669d1f07acfe9d7e5dc957f0edf6b84abaa5e1
gedefay <gedefayeayana@gmail.com>
2022-12-09 05:12:16 -0800

    if while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,16 @@
  * main - Begin of execution
  * Return: 0 if success
 */
+
 int main(void)
+
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
+
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
