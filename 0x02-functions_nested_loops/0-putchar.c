c736ee8f6c4149475ef3177c58258f5a8fb80f14
gedefay <gedefayeayana@gmail.com>
2022-12-13 05:55:14 -0800

    function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,17 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
 
-	return (0);
+int count, sz;
+
+sz = sizeof(str) / sizeof(int);
+
+for (count = 0; count < sz; count++)
+{
+	_putchar(str[count]);
+}
+_putchar('\n');
+
+return (0);
 }
